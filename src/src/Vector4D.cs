33052b524dc3f69322c5131efe6b6e9f26676cdf
Stefan Kewatt <stefan.kewatt@gmail.com>
2020-11-04 01:52:34 -0600

    Added Double versions.

--- src/src/Vector4D.cs
+++ src/src/Vector4D.cs
@@ -14,17 +14,17 @@ namespace MathStructs
         public double W;
 
         public static Vector4D Zero =>
-            new Vector4D(0d);
+            new Vector4D(0f);
         public static Vector4D One =>
-            new Vector4D(1d);
+            new Vector4D(1f);
         public static Vector4D UnitX =>
-            new Vector4D(1d, 0d, 0d, 0d);
+            new Vector4D(1f, 0f, 0f, 0f);
         public static Vector4D UnitY =>
-            new Vector4D(0d, 1d, 0d, 0d);
+            new Vector4D(0f, 1f, 0f, 0f);
         public static Vector4D UnitZ =>
-            new Vector4D(0d, 0d, 1d, 0d);
+            new Vector4D(0f, 0f, 1f, 0f);
         public static Vector4D UnitW =>
-            new Vector4D(0d, 0d, 0d, 1d);
+            new Vector4D(0f, 0f, 0f, 1f);
         public override int GetHashCode() =>
             HashCode.Combine(X.GetHashCode(), Y.GetHashCode(), Z.GetHashCode(), W.GetHashCode());
 
@@ -80,7 +80,20 @@ namespace MathStructs
 
         [MethodImpl(Inline)]
         public static Vector4D Lerp(Vector4D bounds1, Vector4D bounds2, double amount) =>
-            bounds1 * (1d - amount) + bounds2 * amount;
+            bounds1 * (1f - amount) + bounds2 * amount;
+
+        [MethodImpl(Inline)]
+        public static Vector4D Transform(Vector2D position, Matrix4x4F matrix)
+        {
+            return new Vector4D(position.X * matrix.M11 + position.Y * matrix.M21 + matrix.M41, position.X * matrix.M12 + position.Y * matrix.M22 + matrix.M42, position.X * matrix.M13 + position.Y * matrix.M23 + matrix.M43, position.X * matrix.M14 + position.Y * matrix.M24 + matrix.M44);
+        }
+
+        [MethodImpl(Inline)]
+        public static Vector4D Transform(Vector3D vector, Matrix4x4F matrix) =>
+            new Vector4D(vector.X * matrix.M11 + vector.Y * matrix.M21 + vector.Z * matrix.M31 + matrix.M41,
+                         vector.X * matrix.M12 + vector.Y * matrix.M22 + vector.Z * matrix.M32 + matrix.M42,
+                         vector.X * matrix.M13 + vector.Y * matrix.M23 + vector.Z * matrix.M33 + matrix.M43,
+                         vector.X * matrix.M14 + vector.Y * matrix.M24 + vector.Z * matrix.M34 + matrix.M44);
 
         [MethodImpl(Inline)]
         public static Vector4D Transform(Vector4D vector, Matrix4x4F matrix) =>
@@ -88,6 +101,60 @@ namespace MathStructs
                          vector.X * matrix.M12 + vector.Y * matrix.M22 + vector.Z * matrix.M32 + vector.W * matrix.M42,
                          vector.X * matrix.M13 + vector.Y * matrix.M23 + vector.Z * matrix.M33 + vector.W * matrix.M43,
                          vector.X * matrix.M14 + vector.Y * matrix.M24 + vector.Z * matrix.M34 + vector.W * matrix.M44);
+
+        [MethodImpl(Inline)]
+        public static Vector4D Transform(Vector2D value, QuaternionF rotation)
+        {
+            double num = rotation.X + rotation.X;
+            double num2 = rotation.Y + rotation.Y;
+            double num3 = rotation.Z + rotation.Z;
+            double num4 = rotation.W * num;
+            double num5 = rotation.W * num2;
+            double num6 = rotation.W * num3;
+            double num7 = rotation.X * num;
+            double num8 = rotation.X * num2;
+            double num9 = rotation.X * num3;
+            double num10 = rotation.Y * num2;
+            double num11 = rotation.Y * num3;
+            double num12 = rotation.Z * num3;
+            return new Vector4D(value.X * (1f - num10 - num12) + value.Y * (num8 - num6), value.X * (num8 + num6) + value.Y * (1f - num7 - num12), value.X * (num9 - num5) + value.Y * (num11 + num4), 1f);
+        }
+
+        [MethodImpl(Inline)]
+        public static Vector4D Transform(Vector3D value, QuaternionF rotation)
+        {
+            double num = rotation.X + rotation.X;
+            double num2 = rotation.Y + rotation.Y;
+            double num3 = rotation.Z + rotation.Z;
+            double num4 = rotation.W * num;
+            double num5 = rotation.W * num2;
+            double num6 = rotation.W * num3;
+            double num7 = rotation.X * num;
+            double num8 = rotation.X * num2;
+            double num9 = rotation.X * num3;
+            double num10 = rotation.Y * num2;
+            double num11 = rotation.Y * num3;
+            double num12 = rotation.Z * num3;
+            return new Vector4D(value.X * (1f - num10 - num12) + value.Y * (num8 - num6) + value.Z * (num9 + num5), value.X * (num8 + num6) + value.Y * (1f - num7 - num12) + value.Z * (num11 - num4), value.X * (num9 - num5) + value.Y * (num11 + num4) + value.Z * (1f - num7 - num10), 1f);
+        }
+
+        [MethodImpl(Inline)]
+        public static Vector4D Transform(Vector4D value, QuaternionF rotation)
+        {
+            double num = rotation.X + rotation.X;
+            double num2 = rotation.Y + rotation.Y;
+            double num3 = rotation.Z + rotation.Z;
+            double num4 = rotation.W * num;
+            double num5 = rotation.W * num2;
+            double num6 = rotation.W * num3;
+            double num7 = rotation.X * num;
+            double num8 = rotation.X * num2;
+            double num9 = rotation.X * num3;
+            double num10 = rotation.Y * num2;
+            double num11 = rotation.Y * num3;
+            double num12 = rotation.Z * num3;
+            return new Vector4D(value.X * (1f - num10 - num12) + value.Y * (num8 - num6) + value.Z * (num9 + num5), value.X * (num8 + num6) + value.Y * (1f - num7 - num12) + value.Z * (num11 - num4), value.X * (num9 - num5) + value.Y * (num11 + num4) + value.Z * (1f - num7 - num10), value.W);
+        }
 
         [MethodImpl(Inline)]
         public Vector4D Transform(Matrix4x4F matrix) =>
@@ -136,6 +203,50 @@ namespace MathStructs
             W = w;
         }
 
+        public Vector4D(Vector3D vector, double w)
+        {
+            X = vector.X;
+            Y = vector.Y;
+            Z = vector.Z;
+            W = w;
+        }
+
+        public Vector4D(Vector2D vector, double z, double w)
+        {
+            X = vector.X;
+            Y = vector.Y;
+            Z = z;
+            W = w;
+        }
+
+        [MethodImpl(Inline)]
+        public void Deconstruct(out double x, out double y, out double z, out double w)
+        {
+            x = X;
+            y = Y;
+            z = Z;
+            w = W;
+        }
+
+        [MethodImpl(Inline)]
+        public void Deconstruct(out Vector3D vector, out double w)
+        {
+            w = W;
+            vector = new Vector3D(X, Y, Z);
+        }
+
+        [MethodImpl(Inline)]
+        public void Deconstruct(out Vector2D vector, out double z, out double w)
+        {
+            vector = new Vector2D(X, Y);
+            z = Z;
+            w = W;
+        }
+
+        [MethodImpl(Inline)]
+        public Vector4D With(double? x = null, double? y = null, double? z = null, double? w = null) =>
+            new Vector4D(x ?? X, y ?? Y, z ?? Z, w ?? W);
+
         [MethodImpl(Inline)]
         public void CopyTo(double[] array) =>
             CopyTo(array, 0);
@@ -163,6 +274,10 @@ namespace MathStructs
             left.Y * right.Y +
             left.Z * right.Z +
             left.W * right.W;
+
+        [MethodImpl(Inline)]
+        public double Dot(Vector4D vector) =>
+            Dot(this, vector);
 
         [MethodImpl(Inline)]
         public static Vector4D Min(Vector4D left, Vector4D right) =>
@@ -232,6 +347,6 @@ namespace MathStructs
 
         [MethodImpl(Inline)]
         public static bool operator !=(Vector4D left, Vector4D right) =>
-            left.X != right.X && left.Y != right.Y && left.Z != right.Z && left.W != right.W;
+            left.X != right.X || left.Y != right.Y || left.Z != right.Z || left.W != right.W;
     }
 }
