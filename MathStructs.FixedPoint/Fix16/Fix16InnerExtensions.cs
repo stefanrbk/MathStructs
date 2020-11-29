da334d4facf8c7a8db83e0c8d5bb62a51cde6ba7
Stefan Kewatt <stefan.kewatt@gmail.com>
2020-11-29 15:38:23 -0600

    Moved all fixed point code out of MathStructs to allow for independent installs for just FixedPoint and for just the FixedPoint MathStructs.

---
+++ MathStructs.FixedPoint/Fix16/Fix16InnerExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System.Runtime.CompilerServices;
+using System.Runtime.Intrinsics;
+
+namespace MathStructs
+{
+    internal static class Fix16InnerExtensions
+    {
+        public static Vector4Fix16 AsVector4Fix16(this Vector128<int> value) =>
+            Unsafe.As<Vector128<int>, Vector4Fix16>(ref value);
+
+        public static Vector4Fix16 AsVector4Fix16(this Vector128<float> value) =>
+            Unsafe.As<Vector128<float>, Vector4Fix16>(ref value);
+    }
+}
